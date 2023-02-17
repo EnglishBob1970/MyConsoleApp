778114de26a9a80ac74aa5c61726b00751b9aae3
Martin Tittensor <martin.tittensor@gmail.com>
2023-02-17 18:25:28 +0000

    arrow

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -3,6 +3,6 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!! :-)");
-        Console.WriteLine("I added this line!");
+        Console.WriteLine("I added this line <---");
     }
 }
